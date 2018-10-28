1d7ac103e738eccccaf0991ed944b70bef564f60
Andrei Iatsuk <hi@yatsukav.com>
2018-10-28 21:24:49 +0300

    added telegram bot

---
+++ src/main/scala/net/iatsuk/bot/gnugobot/bots/TelegramBot.scala
@@ -0,0 +1,30 @@
+/*
+ * GnuGoBot  Copyright (C) 2018  Andrei Iatsuk (hi@yatsukav.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.iatsuk.bot.gnugobot.bots
+
+import info.mukel.telegrambot4s.api.declarative.Commands
+import info.mukel.telegrambot4s.api.{Polling, TelegramBot}
+
+import scala.io.Source
+
+object TelegramBot extends TelegramBot with Polling with Commands {
+
+  lazy val token = scala.util.Properties
+    .envOrNone("BOT_TOKEN")
+    .getOrElse(Source.fromFile("bot.token").getLines().mkString)
+  onCommand('hello) { implicit msg => reply("My token is work!") }
+}
